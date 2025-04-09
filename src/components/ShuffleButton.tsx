c88a08b3f57d4f4f1c1a6453ace2989ebfbf1282
carolinmimmie <carolinmimmie@gmail.com>
2025-04-09 13:26:31 +0200

    Built component structure and SASS structure files

---
+++ src/components/ShuffleButton.tsx
@@ -0,0 +1,5 @@
+const ShuffleButton = () => {
+  return <div>ShuffleButton</div>;
+};
+
+export default ShuffleButton;
---
+++ src/components/WinMessage.tsx
@@ -0,0 +1,5 @@
+const WinMessage = () => {
+  return <div>WinMessage</div>;
+};
+
+export default WinMessage;
