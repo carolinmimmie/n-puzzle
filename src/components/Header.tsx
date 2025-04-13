de5dae7b0e2add22ca7523c05eb06aafa61f234e
carolinmimmie <carolinmimmie@gmail.com>
2025-04-13 21:57:05 +0200

    framemotion on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,15 @@
+import { motion } from "framer-motion";
 const Header = () => {
-  return <div className="header">Let's Play</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 1.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 3 }}
+      className="header"
+    >
+      Let's Play!
+    </motion.div>
+  );
 };
 
 export default Header;
